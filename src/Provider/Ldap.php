eefcbd8b2547d808fb12bc5eaea979792af9f0be
weijer <chengwei@strack.me>
2018-06-06 20:16:13 +0800

    ldap

--- src/Provider/Ldap.php
+++ src/Provider/Ldap.php
@@ -2,17 +2,53 @@
 
 namespace Provider;
 
+use Adldap\Adldap;
+
 class Ldap
 {
 
-    public function __construct($param)
-    {
+    protected $adldap;
 
+    /**
+     * Ldap constructor.
+     * @param $config
+     */
+    public function __construct($config)
+    {
+        $this->adldap = new Adldap();
+        $this->adldap->addProvider($config);
     }
 
     public function verify()
     {
-        // TODO: Implement getUserData() method.
+        try {
+            // If a successful connection is made to your server, the provider will be returned.
+            $provider = $this->adldap->connect();
+
+            // Performing a query.
+            $results = $provider->search()->where('cn', '=', 'John Doe')->get();
+
+            // Finding a record.
+            $user = $provider->search()->find('jdoe');
+
+            // Creating a new LDAP entry. You can pass in attributes into the make methods.
+            $user = $provider->make()->user([
+                'cn' => 'John Doe',
+                'title' => 'Accountant',
+                'description' => 'User Account',
+            ]);
+
+            // Setting a model's attribute.
+            $user->cn = 'John Doe';
+
+            // Saving the changes to your LDAP server.
+            if ($user->save()) {
+                // User was saved!
+            }
+        } catch (\Adldap\Auth\BindException $e) {
+            // There was an issue binding / connecting to the server.
+
+        }
     }
 
     public function getUserData()
