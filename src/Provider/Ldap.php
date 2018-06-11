9d03bb2ffa1bb601fdfe3df4a4e0544af9a36c67
gkx <szwlqywr.k@gmail.com>
2018-06-11 20:59:07 +0800

    ldap修改

--- src/Provider/Ldap.php
+++ src/Provider/Ldap.php
@@ -23,22 +23,15 @@ class Ldap
         $this->adldap->addProvider($config);
     }
 
-    public function verify()
+    public function verify($login,$password,$config)
     {
-
-
-        $config = ['domain_controllers' => ['192.168.31.126'],
-                   'base_dn'            => 'CN=Administrator,CN=Users,DC=sayms,DC=com',
-                   'admin_username'     => 'CN=Administrator,CN=Users,DC=sayms,DC=com',
-                   'admin_password'     => 'P@ssw0rd',];
+        
         $this->adldap->addProvider($config);
-        $ldapName = 'CN=Administrator,CN=Users,DC=sayms,DC=com';
-        $password = 'P@ssw0rd';
-        $username = 'Administrator';
+
         try {
             $provider = $this->adldap->connect();
             $search   = $provider->search();
-            if ($provider->auth()->attempt($ldapName, $password)) {
+            if ($provider->auth()->attempt($login,$password)) {
                 return true;
             }else{
                 return false;
@@ -98,16 +91,35 @@ class Ldap
 //                       'admin_username'     => 'CN=Administrator,CN=Users,DC=sayms,DC=com',
 //                       'admin_password'     => 'P@ssw0rd',];
 
-        
+
             $this->adldap->addProvider($config);
             try {
+                $ldapName = 'uid=riemann,dc=example,dc=com';
+                $password = 'password';
                 $provider = $this->adldap->connect();
-                $provider->auth()->bindAsAdministrator();
+//                $provider->auth()->bindAsAdministrator();
                 $search   = $provider->search();
+                $record = $search->findByDn('cn=read-only-admin,dc=example,dc=com');
+                $results = $provider->search()->whereHas('cn')->get();
 
-                $record = $search->findBy('samaccountname','1111s');
+//                $record =  $search->select(['cn', 'samaccountname', 'telephone', 'mail']);
+//                $results = $search->all();
+//                $userinfo = $results;
+
+//                    [
+//                    'company'   =>  $record['company'][0],
+//                    'title'     =>  $record['title'][0],
+//                    'telephonenumber' => $record['telephonenumber'][0],
+//                    'mail'      =>  $record['mail'][0],
+//                    'sn'        =>  $record['sn'][0],
+//                    'givenname' =>  $record['givenname'][0],
+//                    'userprincipalname' =>  $record['userprincipalname'][0],
+//                ];
                 echo '<pre>';
-                var_dump($record);
+//                var_dump($results['20']['mail']);
+
+                var_dump($results);
+//                return $results;
             } catch (\Adldap\Auth\BindException $e) {
                echo '连接失败';
             }
