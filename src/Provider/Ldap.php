d98ad78124c729a72f534fbcc2e9f566389c0256
gkx <szwlqywr.k@gmail.com>
2018-07-18 13:44:10 +0800

    ldap完善

--- src/Provider/Ldap.php
+++ src/Provider/Ldap.php
@@ -3,12 +3,13 @@
 namespace StrackOauth\Provider;
 
 use Adldap\Adldap;
+use Adldap\Schemas\ActiveDirectory;
+
 
 class Ldap
 {
 
     protected $adldap;
-
     /**
      * Ldap constructor.
      * @param $config
@@ -18,80 +19,95 @@ class Ldap
         $this->adldap = new Adldap();
     }
 
+    /**
+     * @param $config
+     */
     public function addProvider($config)
     {
         $this->adldap->addProvider($config);
 
     }
 
+    /**
+     * 获得AD名字
+     * @param $config
+     * @return mixed
+     */
+    public function getAdName($config)
+    {
+        $this->adldap->addProvider($config);
+        $provider = $this->adldap->connect();
+        $root = $provider->search()->getRootDse()->getRootDomainNamingContext();
+        $DC=explode(",",$root)["0"];
+        $adName=explode("=",$DC)["1"];
+        return $adName;
+    }
+
+    /**
+     * 验证LDAP参数
+     * @param $param
+     * @param $config
+     * @return bool
+     * @throws \Adldap\Auth\BindException
+     * @throws \Adldap\Auth\PasswordRequiredException
+     * @throws \Adldap\Auth\UsernameRequiredException
+     */
     public function verify($param, $config)
     {
-
         $this->adldap->addProvider($config);
         try {
             $provider = $this->adldap->connect();
-            $search   = $provider->search();
-
-
+            $param["login_name"]=$this->getAdName($config)."\\".$param["login_name"];
             if ($provider->auth()->attempt($param['login_name'], $param['password'])) {
                 return true;
             } else {
                 return false;
             }
         } catch (\Adldap\Models\BindException $e) {
-           echo "Credentials were incorrect";
+            echo "Credentials were incorrect";
         }
     }
 
-    public function ldapUserData($param, $config)
+    /**
+     * 获取单个用户的信息
+     * @param $param
+     * @param $config
+     * @return mixed
+     */
+    public function ldapData($param, $config)
     {
-
         $this->adldap->addProvider($config);
-
         try {
             $provider = $this->adldap->connect();
             $search   = $provider->search();
-            $username = explode("\\",$param['login_name']);
-            $record   = $search->findBy('samaccountname', $username[1]);
-            //uid查找方式
-//            $record = $provider->search()->where("uid", '=', '')->get();
-            $data = [
-                'title'=>$record['title'][0],
-                'department'=>$record['department'][0],
-                'mail'=>$record['mail'][0],
-                'givenname'=> $record['givenname'][0],
-                'sn'=>$record['sn'][0],
-                'cn'=>$record['cn'][0],
-                'description'=>$record['description'][0],
-                'telephonenumber'=>$record['telephonenumber'][0],
-                'company'=>$record['company'][0],
-            ];
-            return $data;
+            //获得根域名
+            $root = $search->getRootDse()->getRootDomainNamingContext();
+            $resData  = $search->findBy('samaccountname', $param["login_name"]);
+            return $resData;
         } catch (\Adldap\Auth\BindException $e) {
             echo 'Credentials were incorrect';
         }
     }
 
-    public function ldapAllUserData($param, $config)
-
+    /**
+     * 获取组中所有信息
+     * @param $param
+     * @param $config
+     * @return array|\Illuminate\Support\Collection
+     */
+    public function ldapAllData($param, $config)
     {
-
-
-        $this->adldap->addProvider($param, $config);
+        //重新拼装配置参数
+        $config["base_dn"] = $param;
+        $this->adldap->addProvider($config);
         try {
             $provider = $this->adldap->connect();
-//            管理员身份绑定登录
-//          $provider->auth()->bindAsAdministrator();
-            $search  = $provider->search();
-//            DN查找
-//            $record  = $search->findByDn('cn=read-only-admin,dc=example,dc=com');
-//            CN查找
-            $results = $provider->search()->whereHas('cn')->get();
-
+            //管理员身份绑定登录
+            $provider->auth()->bindAsAdministrator();
+            $search = $provider->search();
             //all用户获取
-//          $results = $search->all();
-//          $userinfo = $results;
-
+            $results = $search->all();
+            return $results;
         } catch (\Adldap\Auth\BindException $e) {
             echo 'Credentials were incorrect';
         }
